fe32605967c9215fa632bea5240f2476439994dd
Mohamed Mokhtar <mohamed.mahmoud98@eng-st.cu.edu.eg>
2021-07-08 21:39:19 +0200

    Update work.js

--- data/work.js
+++ data/work.js
@@ -1,5 +1,15 @@
 work = [
   {
+    company: "Red Hen Lab | GSoC 2021",
+    title: "Software Engineer |  May 2021 - Aug. 2021 ",
+    details:
+      "I have worked on RapidAnnotator at Red Hen Lab with Google Summer of Code 2021\
+      Rapid Annotator tries  to enable researchers worldwide to annotate large chunks\
+      of data in a very short period of time with the least effort possible and try to\
+      get started with minimal training. My role was adding new features and\
+      fixing bugs on the existing code base",
+  },
+  {
     company: "Senslr Software House",
     title: "Software Engineer |  Sept. 2020 - Nov. 2020 ",
     details:
