4bab0ee1b8a016d5a0366f731026a89e8d841b5b
Mohamed Mokhtar <mohamed.mahmoud98@eng-st.cu.edu.eg>
2021-09-09 21:45:14 +0200

    Update work.js

--- data/work.js
+++ data/work.js
@@ -9,13 +9,13 @@ work = [
       get started with minimal training. My role was adding new features and\
       fixing bugs on the existing code base",
   },
-  {
+  /*{
     company: "Seen Tech",
     title: "Software Engineer |  Aug. 2021 - Present ",
     details:
       "Building web applications and services using Golang with fiber and building user interface pages using React and solving\
       subproblems and modules using another languages.",
-  },
+  },*/
   {
     company: "Seen Tech",
     title: "Software Engineer |  Sept. 2020 - Nov. 2020 ",
